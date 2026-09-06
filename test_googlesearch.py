1fb63b1c8ffc3fd54bd2636a4443d7095a17f4d0
Mikhail Bombin <bober8848@gmail.com>
2026-09-06 16:47:42 +0200

    Added google search negative test

--- test_googlesearch.py
+++ test_googlesearch.py
@@ -9,3 +9,8 @@ def search_page(setup_browser):
 def test_search_positive(search_page):
     browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
     browser.element('html').should(have.text('QA.GURU: Курсы тестировщиков'))
+
+
+def test_search_negative(search_page):
+    browser.element('[name="q"]').should(be.blank).type("'tal'kfnsdodvcnzc;zsmvfcoisefhnasfd;ma;fdma;lfdm").press_enter()
+    browser.element('html').should(have.text('не найдено'))
